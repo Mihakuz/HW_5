783cc914826c6c0bea6269d4e9e1871d2b7ae018
Mikhail Kuznetsov <mihakuz@gmail.com>
2022-01-31 04:52:12 +0300

    HW5

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,8 +1,25 @@
 public class Employee {
-    String name;
-    String position;
-    String email;
-    String phone;
-    int pay;
-    int age;
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int pay;
+    private int age;
+
+    public Employee (String name, String position, String email, String phone, int pay, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.pay = pay;
+        this.age = age;
+    }
+
+    public void printInfo() {
+        System.out.println("ФИО: " + name + "   Должность: " + position + "   e-mail: " + email + "   тел. №: " + "   З/плата: " + pay + "   Возвраст: " + age);
+
+    }
+    public int getAge(){
+        return age;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        Employee[] employeeArr = new Employee[5];
+        employeeArr[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        employeeArr[1] = new Employee("Vasiliy Petrov", "Engineer", "petrovVV@mailbox.com", "89211234567", 32000, 37);
+        employeeArr[2] = new Employee("Ekaterina Gushina", "Bookkeeper", "gushina86@gmail.com", "89993478762", 30000, 35);
+        employeeArr[3] = new Employee("Alexander Matveev", "Security", "AAM78@mail.ru", "89111785236", 25000, 48);
+        employeeArr[4] = new Employee("Gennady Emelyanov", "Director", "Emelyanov_G@mailbox.com", "89052697929", 98000, 41);
+        for (int i = 0; i < employeeArr.length; i++) {
+            if (employeeArr[i].getAge() > 40) employeeArr[i].printInfo();
+        }
+    }
 }
